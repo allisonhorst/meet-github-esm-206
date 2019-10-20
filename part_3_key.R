ec8b5b7f64c194b533db66e561203f44cb7fbf35
Allison Horst <ahorst@ucsb.edu>
2019-10-20 13:11:09 -0700

    update Part 3 with annotate and geom_vline

--- part_3_key.R
+++ part_3_key.R
@@ -18,4 +18,39 @@
 # Attach packages
 
 library(tidyverse)
+library(janitor)
 library(here)
+
+db <- read_csv(here("data","disease_burden.csv")) %>%
+  clean_names() %>%
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+# View(db)
+
+# Subset (US, Japan = lowest infant death rates, Afghanistan = highest infant death rates)
+
+db_sub <- db %>%
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>%
+  filter(age_group == "0-6 days", sex == "Both")
+
+# Graph
+# New things: annotation + vertical line
+
+ggplot(data = db_sub) +
+  geom_line(aes(x = year,
+                 y = deaths_per_100k,
+                 color = country_name)) +
+  scale_color_manual(values = c("black", "blue", "magenta", "orange")) +
+  annotate(geom = "text",
+           x = 1985,
+           y = 2.2e5,
+           label = "Afghanistan",
+           size = 2.5) +
+  geom_vline(xintercept = 2000,
+             lty = 2) +
+  theme_minimal()
+
+# End Part 3
+
+
+
