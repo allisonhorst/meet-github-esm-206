102bf7da6c65ffa8cd69228a6eacf2b875df255c
Allison Horst <ahorst@ucsb.edu>
2019-10-20 12:44:00 -0700

    add part 3 disease burden script

---
+++ part_3_key.R
@@ -0,0 +1,19 @@
+# --------------
+# Part 3 Key
+# ESM 206 Lab 4
+# --------------
+
+# Note: Students will have created a separate
+# git-enabled R project for Part 3
+# In that project, subfolders 'data' and 'final_graphs' created
+# And drop the 'disease_burden.csv' file into 'data'
+
+# That's where this example starts...
+
+# Data from Institute for Health Metrics and Evaluations
+# http://ghdx.healthdata.org/record/ihme-data/gbd-2010-mortality-results-1970-2010
+
+# Attach packages
+
+library(tidyverse)
+library(here)
